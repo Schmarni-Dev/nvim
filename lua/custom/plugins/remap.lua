fe0f79833eb3a600c7afe572e15c840c7f90c798
Schmarni <marnistromer@gmail.com>
2023-07-14 12:26:08 +0200

    idk what changed

--- lua/custom/plugins/remap.lua
+++ lua/custom/plugins/remap.lua
@@ -1,4 +1,5 @@
 vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.keymap.set("t", "<A-Esc>","<C-\\><C-n>")
 -- vim.print("HELLO WORLD!!!")
 return {}
